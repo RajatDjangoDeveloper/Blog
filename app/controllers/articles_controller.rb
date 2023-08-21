641dbda7adb5cbe6d9b610e0ae21e4579afc42e4
rajat j <cseengin@gamil.com>
2023-08-22 03:21:18 +0530

    V1.0

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def index
+    @blogs = Article.all()
+  end
+  def show
+  end
+  def new
+    @employee = Article.new
+  end
+  def create
+    @employee = Article.new(Title, Description) 
+    if @employee.save
+      redirect_to root_path
+    
+    end
+      
+  end
+
+end
